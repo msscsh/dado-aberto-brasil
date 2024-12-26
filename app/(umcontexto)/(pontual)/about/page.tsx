04cf50fb3a2349fcbd1aeba1ee32a56128d0f5f2
msscsh <itsbruno42@gmail.com>
2024-12-26 19:41:24 -0300

    teste

--- app/(umcontexto)/(pontual)/about/page.tsx
+++ app/(umcontexto)/(pontual)/about/page.tsx
@@ -1,3 +1,14 @@
-export default function Page() {
-  return <h1>About</h1>;
+// export default function Page() {
+//   return <h1>About</h1>;
+// }
+export default async function Page() {
+  const data = await fetch('https://api.vercel.app/blog')
+  const posts = await data.json()
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  )
 }
