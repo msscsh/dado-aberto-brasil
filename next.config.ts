0d94d948aae30798787eacbafc3fd1ee354a8025
msscsh <itsbruno42@gmail.com>
2024-12-26 17:02:03 -0300

    testes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/dado-aberto-brasil",
   output: "export",  // <=== enables static exports
   /* config options here */
 };
