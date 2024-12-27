3ced13acb0a1e980057521f6cb2a99267e1c022b
msscsh <itsbruno42@gmail.com>
2024-12-27 12:51:28 -0300

    testes

--- app/(testes)/(fetch-api)/list/page.tsx
+++ app/(testes)/(fetch-api)/list/page.tsx
@@ -1,5 +1,6 @@
 export default async function Page() {
-  const data = await fetch('http://localhost:8080/posts')
+  // const data = await fetch('http://localhost:8080/posts')
+  const data = await fetch('https://api.vercel.app/blog')
   const posts = await data.json()
   return (
     <ul>
