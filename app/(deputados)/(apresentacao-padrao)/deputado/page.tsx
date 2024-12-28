8016afa732b9e26f0ca4e766d45af84341244a57
msscsh <itsbruno42@gmail.com>
2024-12-28 15:31:43 -0300

    apresentacao e2e

--- app/(deputados)/(apresentacao-padrao)/deputado/page.tsx
+++ app/(deputados)/(apresentacao-padrao)/deputado/page.tsx
@@ -1,26 +1,17 @@
-import { promises as fs } from 'fs';
 import Image from 'next/image'
 
-export default async function MyComponent() {
+export default function DeputadoCard({ deputadoAtual = {} }) {
 
-    const file = await fs.readFile(process.cwd() + '/public/list_deputados.json', 'utf8');
-    const data = JSON.parse(file);
-
-    return (
-        <div>
-            {data.map((item) => (
-                  <div key={item.ideCadastro}>
-                    <Image
-                      src={item.urlFoto}
-                      width={80}
-                      height={100}
-                      alt={item.nome}
-                    />
-                    <p key={item.ideCadastro}>{item.nome}</p>
-                  </div>
-            ))}
-        </div>
-    );
+  return (
+    <div style={{display: "inline", justifyContent: "center", width: "40%"}}>
+      <Image
+        src={deputadoAtual.urlFoto}
+        width={80}
+        height={100}
+        alt={deputadoAtual.nome}
+      />
+    </div>
+  );
 }
 
 
